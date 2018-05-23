5ce8108aeae79671e6b95dd86575b2242e01a3b9
FJJI <fjjimenez@miuandes.cl>
2018-05-22 21:53:13 -0400

    UML

---
+++ aaa/Interfaz/Login_Window.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz
+{
+    public partial class Login_Window : Form
+    {
+        private Iloger listener;
+        public interface Iloger
+        {
+            void Login(Usuario usuario);
+        }
+        public Login_Window(object sender)
+        {
+            InitializeComponent();
+            if (sender is Iloger)
+            {
+                listener = (Iloger)sender;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = textBox_user.Text;
+            string claveUsuario = textBox_password.Text;
+            Usuario usuario = new Usuario(nombreUsuario, claveUsuario);
+            listener.Login(usuario);
+            this.Close();
+        }
+    }
+}
---
+++ aaa/Interfaz/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaz
+{
+    public class Usuario
+    {
+        public string nombre;
+        public string password;
+
+        public Usuario(string nombre, string password)
+        {
+            this.nombre = nombre;
+            this.password = password;
+        }
+    }
+}
